70405912232f583c845fa5e240691711ebbc0474
n0rs <n0rs9485@googlemail.com>
2025-06-29 10:48:30 +0200

    minor

--- src/dataaccesslayer/DatenbankManager.java
+++ src/dataaccesslayer/DatenbankManager.java
@@ -46,7 +46,7 @@ public class DatenbankManager extends UnicastRemoteObject implements DatenbankMa
 
     private static String URL = "jdbc:postgresql://localhost/AD2-Projekt";
     private static String BENUTZERNAME = "postgres";
-    private static String PASSWORT = "Davidvilla12*";
+    private static String PASSWORT = "";
 
     // Verbindung zur Datenbank
     private static Connection connection;
