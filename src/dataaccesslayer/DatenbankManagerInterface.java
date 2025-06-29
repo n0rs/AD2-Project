0b0bbbe0c5234a2e7dc96314bafc694cf6944a58
n0rs <n0rs9485@googlemail.com>
2025-06-29 12:32:09 +0200

    abstract

--- src/dataaccesslayer/DatenbankManagerInterface.java
+++ src/dataaccesslayer/DatenbankManagerInterface.java
@@ -5,18 +5,18 @@ import businesslayer.objekte.Kunde;
 import java.rmi.RemoteException;
 
 public interface DatenbankManagerInterface extends java.rmi.Remote {
-    public abstract void verbindungAufbauen() throws RemoteException;
-    public abstract void verbindungTrennen() throws RemoteException;
-    public abstract void kundeAnlegen(String email, String passwort) throws RemoteException;
-    public abstract void kundenLoeschenId(int user_id) throws RemoteException;
-    public abstract void emailVerificationEintragErstellen(int kundenId, String token) throws RemoteException;
-    public abstract void passwortResetEintragErstellen(int kundenId, String token) throws RemoteException;
-    public abstract Kunde findeKundeNachEmail(String email) throws RemoteException;
-    public abstract String findeEmailTokenMitEmail(String email) throws RemoteException;
-    public abstract String findePasswortTokenMitEmail(String email) throws RemoteException;
-    public abstract void updateStatus(int user_id, boolean isActive) throws RemoteException;
-    public abstract void updatePassword(int user_id, String newPassword) throws RemoteException;
-    public abstract Kunde findeKundeNachID(int user_id) throws RemoteException;
-    public abstract void loescheEmailToken(int kundenId) throws RemoteException;
-    public abstract void loeschePasswortToken(int kundenId) throws RemoteException;
+    public void verbindungAufbauen() throws RemoteException;
+    public void verbindungTrennen() throws RemoteException;
+    public void kundeAnlegen(String email, String passwort) throws RemoteException;
+    public void kundenLoeschenId(int user_id) throws RemoteException;
+    public void emailVerificationEintragErstellen(int kundenId, String token) throws RemoteException;
+    public void passwortResetEintragErstellen(int kundenId, String token) throws RemoteException;
+    public Kunde findeKundeNachEmail(String email) throws RemoteException;
+    public String findeEmailTokenMitEmail(String email) throws RemoteException;
+    public String findePasswortTokenMitEmail(String email) throws RemoteException;
+    public void updateStatus(int user_id, boolean isActive) throws RemoteException;
+    public void updatePassword(int user_id, String newPassword) throws RemoteException;
+    public Kunde findeKundeNachID(int user_id) throws RemoteException;
+    public void loescheEmailToken(int kundenId) throws RemoteException;
+    public void loeschePasswortToken(int kundenId) throws RemoteException;
 }
